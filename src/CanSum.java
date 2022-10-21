ab129efa094cc5b3fbaaf7a23e125286b6c65021
Ahmad Omari <alomari.ah98@gmail.com>
2022-10-21 20:06:07 +0300

    feat: implement can sum method

--- src/CanSum.java
+++ src/CanSum.java
@@ -31,10 +31,10 @@ public class CanSum {
     }
 
     public static void main(String[] args) {
-        System.out.println(canSum(7, new int[]{2, 3}, new HashMap<>())); // true
-        System.out.println(canSum(7, new int[]{5, 3, 4, 7}, new HashMap<>())); // true
-        System.out.println(canSum(7, new int[]{2, 4}, new HashMap<>())); // false
-        System.out.println(canSum(8, new int[]{2, 3, 5}, new HashMap<>())); // true
-        System.out.println(canSum(300, new int[]{7, 14}, new HashMap<>())); // false
+        System.out.println(canSum(7, new int[]{2, 3}, new HashMap<>()));
+        System.out.println(canSum(7, new int[]{5, 3, 4, 7}, new HashMap<>()));
+        System.out.println(canSum(7, new int[]{2, 4}, new HashMap<>()));
+        System.out.println(canSum(8, new int[]{2, 3, 5}, new HashMap<>()));
+        System.out.println(canSum(300, new int[]{7, 14}, new HashMap<>()));
     }
 }
