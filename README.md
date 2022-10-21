afdb4bfa5c2f859488f3f9588b940ee53ad4a375
Ahmad Omari <alomari.ah98@gmail.com>
2022-10-21 20:04:10 +0300

    feat: implement best sum method

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# dynamic-programming-memoization-can-sum
-dynamic-programming-memoization-can-sum
+# Dynamic Programming Memoization "Can Sum"
+
+The function ‘canSum(targetSum, numbers, memo)‘ that takes in a
+targetSum of Integer, array of numbers and a memo map as arguments.
+
+* The function returns a boolean indicating whether it is possible to generate the targetSum using numbers from the array.
+* Using an element of the array multiple times is allowed. 
+* Notice that all input numbers are non-negative numbers.
+
+# Complexity
+
+###Assume that:
+#####m = targetSum
+#####n = numbers array length
+###
+####Time Complexity is O( n * m )
+####Space Complexity is O( m )
---
+++ src/CanSum.java
@@ -0,0 +1,40 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author : Ahmad Al-Omari
+ * @mailto : alomari.ah98@gmail.com
+ * @created : 10/21/2022, Friday
+ * @project : dynamic-programming-memoization-can-sum
+ **/
+public class CanSum {
+
+    public static boolean canSum(int targetSum, int[] numbers, Map<Integer, Boolean> memo){
+        if(memo.containsKey(targetSum)){
+            return memo.get(targetSum);
+        }
+        if (targetSum == 0){
+            return true;
+        }
+        if (targetSum < 0){
+            return false;
+        }
+        for (int num : numbers) {
+            int reminder = targetSum - num;
+            if (canSum(reminder, numbers, memo)) {
+                memo.put(targetSum, true);
+                return true;
+            }
+        }
+        memo.put(targetSum, false);
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(canSum(7, new int[]{2, 3}, new HashMap<>())); // true
+        System.out.println(canSum(7, new int[]{5, 3, 4, 7}, new HashMap<>())); // true
+        System.out.println(canSum(7, new int[]{2, 4}, new HashMap<>())); // false
+        System.out.println(canSum(8, new int[]{2, 3, 5}, new HashMap<>())); // true
+        System.out.println(canSum(300, new int[]{7, 14}, new HashMap<>())); // false
+    }
+}
